692ab75c60ae3287302e052c70d2049f8d8999a1
Hafeed <Hafeed06@gmail.com>
2021-10-17 01:13:46 +0200

    Security Patch

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ import postRoutes from "./routes/posts.js"
 // Deprecated -- import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import { CONNECTION_URL, PORT } from "./credentials.js";
+// import { CONNECTION_URL, PORT } from "./credentials.js";
+import dotenv from 'dotenv'; 
+
 
 /** Initializing express */
 
 const app = express(); 
+dotenv.config(); 
 
 
 
@@ -16,6 +19,9 @@ app.use(express.urlencoded({limit:"30mb", extended: true}))
 app.use(cors()); 
 app.use("/posts", postRoutes); 
 
-mongoose.connect(CONNECTION_URL)
+const PORT = process.env.PORT || 5000; 
+
+
+mongoose.connect(process.env.CONNECTION_URL)
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message))
